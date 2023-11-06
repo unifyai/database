44772dfc646696714ae999d9dca6d6fec8592480
Kareem Morsy <kreem.morsy@hotmail.com>
2023-11-06 17:31:02 +0200

    feat: Add JSON database generation

---
+++ scripts/database_generator.py
@@ -0,0 +1,85 @@
+import yaml
+import json
+import os
+import logging
+import argparse
+import re
+
+TAGS_PATH = "tags.yaml"
+strict = False
+
+
+def log_warning(msg: str, exception_class: Exception = Exception):
+    logging.warning(msg)
+    if strict:
+        raise exception_class(msg)  # pylint: disable=broad-exception-raised
+
+
+def load_tags() -> list[str]:
+    with open(TAGS_PATH, "r", encoding="utf-8") as f:
+        tags = yaml.safe_load(f)
+    tags = tags["tags"]
+
+    logging.info("Loaded tags")
+
+    # Find duplicate tags
+    tags_set = set(tags)
+    if len(tags_set) != len(tags):
+        log_warning("Duplicate tags found", ValueError)
+        tags = list(tags_set)
+
+    return tags
+
+
+def check_tags(tags: list[str], entry_tags: list[str]):
+    tags_set = set(entry_tags)
+    if len(tags_set) != len(entry_tags):
+        log_warning("Duplicate tags found", ValueError)
+        entry_tags = list(tags_set)
+
+    for tag in entry_tags:
+        if tag not in tags:
+            log_warning(f"Tag {tag} not found", ValueError)
+
+
+def load_database(tags: list[str]) -> dict[str, list[str]]:
+    database = {}
+    for root, _, files in os.walk("."):
+        for file in files:
+            if file != TAGS_PATH and re.match(r"[^.].*\.ya?ml$", file):
+                logging.debug("Loading %s", os.path.join(root, file))
+                with open(os.path.join(root, file), "r", encoding="utf-8") as f:
+                    entry = yaml.safe_load(f)
+                    check_tags(tags, list(entry.values())[0]["tags"])
+
+                    database.update(entry)
+
+    logging.info("Loaded database")
+    return database
+
+
+def main():
+    tags = load_tags()
+    database = load_database(tags)
+
+    os.makedirs("build", exist_ok=True)
+    with open("build/database.json", "w", encoding="utf-8") as f:
+        json.dump(database, f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s", "--strict", action="store_true", help="Fail in case of warnings"
+    )
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
+
+    args = parser.parse_args()
+    strict = args.strict
+
+    logging.basicConfig(
+        level=logging.DEBUG if args.verbose else logging.INFO,
+        format="%(levelname)s: %(message)s",
+    )
+
+    main()
